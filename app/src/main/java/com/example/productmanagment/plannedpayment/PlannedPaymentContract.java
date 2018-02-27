b8909d09d9402ce870d5d5e88498e5b41240775a
Ivan Tsiupa <vattgert@gmail.com>
2018-02-27 17:27:29 +0200

    planned payment module created

--- app/src/main/java/com/example/productmanagment/plannedpayment/PlannedPaymentContract.java
+++ app/src/main/java/com/example/productmanagment/plannedpayment/PlannedPaymentContract.java
@@ -1,8 +1,29 @@
 package com.example.productmanagment.plannedpayment;
 
+import com.example.productmanagment.BasePresenter;
+import com.example.productmanagment.BaseView;
+import com.example.productmanagment.data.models.PlannedPayment;
+
+import java.util.List;
+
 /**
  * Created by Ivan on 25.02.2018.
  */
 
 public interface PlannedPaymentContract {
+    interface View extends BaseView<Presenter> {
+
+        void setLoadingIndicator(boolean active);
+        void showPlannedPayments(List<PlannedPayment> plannedPayments);
+        void showAddPlannedPayment();
+        void showLoadingPlannedPaymentsError();
+        void showNoPlannedPayments();
+        void showPlannedPaymentSuccessfullySavedMessage();
+    }
+
+    interface Presenter extends BasePresenter {
+        void loadPlannedPayments();
+        void addPlannedPayments();
+        void result(int requestCode, int resultCode);
+    }
 }
--- app/src/main/java/com/example/productmanagment/plannedpayment/PlannedPaymentPresenter.java
+++ app/src/main/java/com/example/productmanagment/plannedpayment/PlannedPaymentPresenter.java
@@ -4,5 +4,29 @@ package com.example.productmanagment.plannedpayment;
  * Created by Ivan on 25.02.2018.
  */
 
-public class PlannedPaymentPresenter {
+public class PlannedPaymentPresenter implements PlannedPaymentContract.Presenter {
+    @Override
+    public void subscribe() {
+
+    }
+
+    @Override
+    public void unsubscribe() {
+
+    }
+
+    @Override
+    public void loadPlannedPayments() {
+
+    }
+
+    @Override
+    public void addPlannedPayments() {
+
+    }
+
+    @Override
+    public void result(int requestCode, int resultCode) {
+
+    }
 }
